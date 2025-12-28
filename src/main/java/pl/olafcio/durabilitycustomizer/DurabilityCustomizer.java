f4e57f6703f7b69c0e8e0052c4c47883d803c770
Olafcio <75330538+Olafcio1@users.noreply.github.com>
2025-12-28 18:52:43 +0100

    Downgrade to 1.16.5

--- src/main/java/pl/olafcio/durabilitycustomizer/DurabilityCustomizer.java
+++ src/main/java/pl/olafcio/durabilitycustomizer/DurabilityCustomizer.java
@@ -1,45 +1,45 @@
 package pl.olafcio.durabilitycustomizer;
 
 import com.destroystokyo.paper.event.block.AnvilDamagedEvent;
-import io.papermc.paper.command.brigadier.Commands;
-import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents;
 import net.kyori.adventure.text.Component;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerItemDamageEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import static com.mojang.brigadier.Command.SINGLE_SUCCESS;
+import org.jetbrains.annotations.NotNull;
 
 public final class DurabilityCustomizer extends JavaPlugin implements Listener {
     FileConfiguration config;
 
     @Override
-    @SuppressWarnings("UnstableApiUsage")
     public void onEnable() {
         config = getConfig();
-
         getServer().getPluginManager().registerEvents(this, this);
-        getLifecycleManager().registerEventHandler(LifecycleEvents.COMMANDS, commands -> {
-            commands.registrar().register(Commands.literal("durabilitycustomizer")
-                            .executes(ctx -> {
-                                ctx.getSource().getSender().sendMessage(Component.text("§3[DurabilityCustomizer]§7 Made by §aOlafcio§7 with §blove"));
-                                return SINGLE_SUCCESS;
-                            })
-                            .then(
-                                Commands.literal("reload")
-                                .executes(ctx -> {
-                                    reloadConfig();
-                                    config = getConfig();
+    }
 
-                                    ctx.getSource().getSender().sendMessage(Component.text("§3[DurabilityCustomizer]§7 Reloaded the configuration."));
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if (command.getName().equals("durabilitycustomizer")) {
+            if (args.length >= 1 && args[0].equals("reload")) {
+                if (sender.hasPermission("durabilitycustomizer.reload")) {
+                    reloadConfig();
+                    config = getConfig();
 
-                                    return SINGLE_SUCCESS;
-                                })
-                            )
-            .build());
-        });
+                    sender.sendMessage(Component.text("§3[DurabilityCustomizer]§7 Reloaded the configuration."));
+                } else {
+                    sender.sendMessage(Component.text("§3[DurabilityCustomizer]§cError:§4 No permission."));
+                }
+            } else {
+                sender.sendMessage(Component.text("§3[DurabilityCustomizer]§7 Made by §aOlafcio§7 with §blove"));
+            }
+
+            return true;
+        }
+
+        return false;
     }
 
     @EventHandler
